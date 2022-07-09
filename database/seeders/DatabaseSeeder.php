683e9c492c7822388ac1bb0fc018800e739450dc
alphaxadLaravel <alphaxadfundi@gmail.com>
2022-07-09 22:35:13 +0300

    admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +17,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
+        // \App\Models\User::factory()->create([
+           
+        // ]);
+
+        DB::table('users')->insert([
             'username' => 'admin',
             'fullname' => 'admin',
             'role' => 'admin',
